c65a6cffb61a2fe479de4e95db5fbf7e37bc4d1c
Devraj Kumar <devrajkumar1903@gmail.com>
2022-01-07 09:54:30 +0530

    Update DistributingMitemsInCircle.cpp

--- Company-1/DistributingMitemsInCircle.cpp
+++ Company-1/DistributingMitemsInCircle.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+  public:
+    int findPosition(int n , int m , int k) {
+        // code here
+        m = m%n;
+        k--;
+        k = (k+m-1)%n;
+        return k+1;
+    }
+};
