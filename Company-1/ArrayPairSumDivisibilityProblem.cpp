cb3c6ed967e3a71ef86bb4c2d580e9a27ac87cad
Devraj Kumar <devrajkumar1903@gmail.com>
2022-01-08 14:33:03 +0530

    Update ArrayPairSumDivisibilityProblem.cpp

--- Company-1/ArrayPairSumDivisibilityProblem.cpp
+++ Company-1/ArrayPairSumDivisibilityProblem.cpp
@@ -1,1 +1,21 @@
-
+bool canPair(vector<int> nums, int k) {
+        // Code here.
+        if(nums.size()%2)
+        return false;
+        int hash[k]={0};
+        for(auto x : nums){
+            hash[x%k]++;
+        }
+        int i;
+        int x = k/2;
+        if(k%2 == 0){
+            if(hash[x]%2)
+            return false;
+        x--;
+        }
+        for(i=1;i<=x;i++){
+            if(hash[i] != hash[k-i])
+            return false;
+        }
+        return true;
+    }
