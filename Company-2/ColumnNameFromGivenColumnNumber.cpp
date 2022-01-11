6d7909132a871d146f7bd833ebb90164d9025890
Devraj Kumar <devrajkumar1903@gmail.com>
2022-01-11 10:08:08 +0530

    Update ColumnNameFromGivenColumnNumber.cpp

--- Company-2/ColumnNameFromGivenColumnNumber.cpp
+++ Company-2/ColumnNameFromGivenColumnNumber.cpp
@@ -1,1 +1,16 @@
-
+class Solution{
+    public:
+    #define ll long long int
+    string colName (long long int n)
+    {
+        // your code here
+       string s = "";
+       while(n > 0){
+           n--;
+           s += char(n%26 + 65);
+           n /= 26;
+       }
+       reverse(s.begin(),s.end());
+       return s;
+    }
+};
