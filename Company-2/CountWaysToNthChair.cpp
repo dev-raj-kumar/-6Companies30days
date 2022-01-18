30602642588ddc1a31e2b8d46ba32b2b1f44f87c
Devraj Kumar <devrajkumar1903@gmail.com>
2022-01-18 18:28:55 +0530

    Update CountWaysToNthChair.cpp

--- Company-2/CountWaysToNthChair.cpp
+++ Company-2/CountWaysToNthChair.cpp
@@ -1,1 +1,18 @@
-
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        long long dp[m+1];
+        int i;
+        dp[0]=dp[1]=1;
+        for(i=2;i<=m;i++){
+            dp[i]=1;
+            dp[i] += dp[i-2];
+        }
+        return dp[m];
+    }
+};
